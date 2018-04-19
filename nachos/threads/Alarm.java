09c7313df1d1740d63177fde921c82dcaf8cbb17
Andrew Sanchez <ans046@ucsd.edu>
2018-04-18 23:44:06 -0700

    syncing up repo

---
+++ nachos/threads/Alarm.java
@@ -0,0 +1,88 @@
+package nachos.threads;
+import java.util.PriorityQueue;
+import nachos.machine.*;
+
+/**
+ * Uses the hardware timer to provide preemption, and to allow threads to sleep
+ * until a certain time.
+ */
+public class Alarm {
+
+    private PriorityQueue<ThreadTime> waitQ;  //
+
+	/**
+	 * Allocate a new Alarm. Set the machine's timer interrupt handler to this
+	 * alarm's callback.
+	 * 
+	 * <p>
+	 * <b>Note</b>: Nachos will not function correctly with more than one alarm.
+	 */
+	public Alarm() {
+		Machine.timer().setInterruptHandler(new Runnable() {
+			public void run() {
+				timerInterrupt();
+			}
+		});
+	}
+
+	/**
+	 * The timer interrupt handler. This is called by the machine's timer
+	 * periodically (approximately every 500 clock ticks). Causes the current
+	 * thread to yield, forcing a context switch if there is another thread that
+	 * should be run.
+	 */
+	public void timerInterrupt() {
+		long curTime = Machine.timer().getTime();
+	    KThread.currentThread().yield();
+	}
+
+	/**
+	 * Put the current thread to sleep for at least <i>x</i> ticks, waking it up
+	 * in the timer interrupt handler. The thread must be woken up (placed in
+	 * the scheduler ready set) during the first timer interrupt where
+	 * 
+	 * <p>
+	 * <blockquote> (current time) >= (WaitUntil called time)+(x) </blockquote>
+	 * 
+	 * @param x the minimum number of clock ticks to wait.
+	 * 
+	 * @see nachos.machine.Timer#getTime()
+	 */
+	public void waitUntil(long x) {
+		// for now, cheat just to get something working (busy waiting is bad)
+		long wakeTime = Machine.timer().getTime() + x;
+		KThread thread = KThread.currentThread();
+		while (wakeTime > Machine.timer().getTime())
+			KThread.yield();
+	}
+
+	// private class created to define thread time objects that can be placed and sorted in queue
+    private class ThreadTime implements Comparable<ThreadTime> {
+	    private KThread thread;
+	    private long waketime;
+
+	    public ThreadTime (KThread thread, long waketime) {
+            this.thread = thread;
+            this.waketime = waketime;
+        }
+
+        /**
+         * Compares object with specified object for order
+         *
+         * @param threadTime
+         * @return 1 if current thread's waketime is greater than specified thread.
+         *         -1 if current thread's waketime is less than specified thread.
+         *         0 thread's waketime is equal to specified threads waittime
+         */
+        public int compareTo(ThreadTime threadTime) {
+	        if( this.waketime > threadTime.waketime ) {
+	            return 1;
+            }else if( this.waketime < threadTime.waketime ) {
+	            return -1;
+            }
+            else {
+	            return 0;
+	        }
+        }
+    }
+}
